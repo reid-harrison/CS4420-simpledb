21ba285e3cd48d449c71ec8c612453ed9a66f8c6
Jeff Drasher <jeff.drasher@gmail.com>
2013-04-11 19:24:58 -0400

    optimizer branch and stub

--- src/gt/cs4420/relationaldb/database/query/QueryOptimizer.java
+++ src/gt/cs4420/relationaldb/database/query/QueryOptimizer.java
@@ -1,4 +1,11 @@
 package gt.cs4420.relationaldb.database.query;
 
+import org.antlr.runtime.tree.CommonTree;
+
 public class QueryOptimizer {
+
+    public CommonTree optimize(CommonTree queryTree){
+        return queryTree;
+    }
+
 }
