4b6ae0af027b9e22785128fbb0d4837e0857a884
Jeff Drasher <jeff.drasher@gmail.com>
2013-04-16 18:00:44 -0400

    added method stubs, test case skeleton

--- src/gt/cs4420/relationaldb/database/query/QueryOptimizer.java
+++ src/gt/cs4420/relationaldb/database/query/QueryOptimizer.java
@@ -1,11 +1,28 @@
 package gt.cs4420.relationaldb.database.query;
 
+import gt.cs4420.relationaldb.database.storage.StorageStatistics;
 import org.antlr.runtime.tree.CommonTree;
 
 public class QueryOptimizer {
 
+    private final StorageStatistics statistics;
+
+    public  QueryOptimizer(StorageStatistics statistics) {
+        this.statistics = statistics;
+    }
+
     public CommonTree optimize(CommonTree queryTree){
+        restructureLogicalPlan(queryTree);
+        generatePhysicalPlan(queryTree);
         return queryTree;
+    }
+
+    private void restructureLogicalPlan(CommonTree queryTree) {
+
+    }
+
+    private void generatePhysicalPlan(CommonTree queryTree) {
+
     }
 
 }
--- test/gt/cs4420/relationaldb/database/query/QueryOptimizeTest.java
+++ test/gt/cs4420/relationaldb/database/query/QueryOptimizeTest.java
@@ -1,17 +1,25 @@
 package gt.cs4420.relationaldb.database.query;
 
+import gt.cs4420.relationaldb.database.storage.StorageStatistics;
+import org.antlr.runtime.tree.CommonTree;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class QueryOptimizeTest {
 
     @Test
     public void optimizationTest(){
-        QueryOptimizer optimizer = new QueryOptimizer();
+        QueryOptimizer optimizer = new QueryOptimizer(mock(StorageStatistics.class));
+
+        CommonTree queryTree = mock(CommonTree.class);
+        CommonTree optimizedtree = optimizer.optimize(queryTree); //may need real value over mock object here
 
         assertThat(optimizer, is(notNullValue()));
+        assertThat(queryTree, is(sameInstance(queryTree)));
     }
 }
