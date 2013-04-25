6ac60b99852a60f347b413286f745100506f4007
reid-harrison <rharr90@gmail.com>
2013-04-24 22:09:50 -0400

    Making StorageStatistics a singleton

--- src/gt/cs4420/relationaldb/database/storage/StorageStatistics.java
+++ src/gt/cs4420/relationaldb/database/storage/StorageStatistics.java
@@ -2,6 +2,20 @@ package gt.cs4420.relationaldb.database.storage;
 
 public class StorageStatistics {
 
+    private static StorageStatistics instance;
+
+    public StorageStatistics getInstance() {
+        if (instance == null) {
+            instance = new StorageStatistics();
+        }
+
+        return instance;
+    }
+
+    private StorageStatistics() {
+
+    }
+
     public int tuplesInRelation(String table){
         return 0;
     }
