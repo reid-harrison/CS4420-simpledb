0b667d52cf8099fe2f5109002f7f4f6617e2f0d4
reid-harrison <rharr90@gmail.com>
2013-04-25 14:39:47 -0400

    More work on runner class

--- src/gt/cs4420/relationaldb/run/DatabaseRunner.java
+++ src/gt/cs4420/relationaldb/run/DatabaseRunner.java
@@ -3,10 +3,16 @@ package gt.cs4420.relationaldb.run;
 import gt.cs4420.relationaldb.database.query.QueryEngine;
 import gt.cs4420.relationaldb.database.query.QueryParser;
 import gt.cs4420.relationaldb.database.storage.StorageManager;
+import gt.cs4420.relationaldb.domain.JoinedRow;
+import gt.cs4420.relationaldb.domain.Row;
 import gt.cs4420.relationaldb.domain.exception.ValidationException;
 
 import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.tree.BaseTree;
+import org.antlr.runtime.tree.CommonTree;
+import org.antlr.runtime.tree.Tree;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class DatabaseRunner {
@@ -63,13 +69,69 @@ public class DatabaseRunner {
 
                 continue;
 			}
-            
 
-            //TODO Run query and display results
+            CommonTree queryTree = parser.getQueryTree();
+
+            if (isSelect(queryTree)) {
+                if (isJoin(queryTree)) {
+                    List<JoinedRow> returnedRows = queryEngine.selectFromJoinedTables(parser);
+                    printJoinedRows(returnedRows);
+                    continue;
+                } else {
+                    List<Row> returnedRows = queryEngine.selectFromTable(parser);
+                    printRows(returnedRows);
+                    continue;
+                }
+            } else {
+                boolean success = false;
+                
+                try {
+                    success = queryEngine.executeQuery(queryTree);
+
+                    if (success) {
+                        System.out.println("Query completed successfully!");
+                    } else {
+                        System.out.println("Query completed unsuccessfully...");
+                    }
+
+                } catch (final ValidationException ve) {
+                    for (String msg : ve.getMessages()) {
+                        System.out.println(msg);
+                    }
+
+                    continue;
+                }
+            }
         }
     }
 
     private void parseQuery(final String query) throws RecognitionException, ValidationException {
         parser = new QueryParser(query);
+    }
+
+    private boolean isSelect(final CommonTree query) {
+        if("SELECT".equals(query.getChild(0).getText())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isJoin(final CommonTree query) {
+        Tree fromRoot = query.getChild(1);
+
+        if ("JOIN".equals(fromRoot.getChild(0))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void printJoinedRows(final List<JoinedRow> rows) {
+
+    }
+
+    private void printRows(final List<Row> rows) {
+
     }
 }
